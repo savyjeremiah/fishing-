39717c3437848c7821a491afc48f0913cb04d01d
saviour <saviourjeremiah2024@gmail.com>
2025-03-26 15:28:53 -0700

    sch

--- ecommerce/ecommerce/wsgi.py
+++ ecommerce/ecommerce/wsgi.py
@@ -7,8 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'house.settings')
 
 application = get_wsgi_application()
+app =application
